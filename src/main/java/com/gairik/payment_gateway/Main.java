e2494d6bd1e649af2c0fe55ac016bb451ea23f64
Sk1997 <sksingh@gairik.in>
2025-09-28 13:01:11 +0530

    1st commit on 28th september

---
+++ src/main/java/com/gairik/payment_gateway/Main.java
@@ -0,0 +1,9 @@
+package com.gairik.payment_gateway;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Documenting DSA by Striver for education purpose");
+        }
+    }
